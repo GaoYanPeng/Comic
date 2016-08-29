bae0c937758179a5998c60914d2543c484783195
DeceiverLiu <dllo@dllodeMac-mini-272.local>
2016-08-29 11:38:26 +0800

    ik

--- come/app/src/main/java/com/lanou3g/come/MainActivity.java
+++ come/app/src/main/java/com/lanou3g/come/MainActivity.java
@@ -2,9 +2,12 @@ package com.lanou3g.come;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private ImageView ik;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
