28c98fa7c9894ce3fe0030eea1dae9c3410cff44
高鹏 <1598039446@qq.com>
2016-08-29 11:21:12 +0800

    功能1

--- come/app/src/main/java/com/lanou3g/come/MainActivity.java
+++ come/app/src/main/java/com/lanou3g/come/MainActivity.java
@@ -2,12 +2,19 @@ package com.lanou3g.come;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //第一
     }
 }
