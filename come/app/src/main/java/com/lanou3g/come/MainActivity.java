9d554d2e26b72c3ec2b6f594b182ded4f13a1514
高鹏 <1598039446@qq.com>
2016-08-29 11:51:09 +0800

    组长修改2

--- come/app/src/main/java/com/lanou3g/come/MainActivity.java
+++ come/app/src/main/java/com/lanou3g/come/MainActivity.java
@@ -2,10 +2,12 @@ package com.lanou3g.come;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.ImageView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     private ImageView ik;
     @Override
@@ -13,5 +15,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //uuuuuuu
+    }
+
+    @Override
+    public void onClick(View view) {
+        //组长修改
     }
 }
